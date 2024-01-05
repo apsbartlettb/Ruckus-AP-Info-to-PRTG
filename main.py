6bed4a0d9778ae5d77274c089abf25d90c32f4f2
Blake Bartlett <bartlettb@alpenaschools.com>
2024-01-05 08:31:41 -0500

    Cleaned up the code a bit.

--- main.py
+++ main.py
@@ -7,17 +7,12 @@ urllib3.disable_warnings()
 
 load_dotenv()
 
+# "https://0.0.0.0:8443/wsg/api/public/v11_1"
 api_base_url = os.getenv("API_URL_BASE")
 
 serviceTicket = ""
 st_file = "serviceTicket.txt"
 
-# if os.path.exists(st_file):
-#     with open(st_file, 'r') as file:
-#         serviceTicket = file.read().strip()
-#     # print(f"Loaded serviceTicket {serviceTicket} from {st_file}")
-
-# controllerVersion = ""
 apsZoneUID = ""
     
 def api_login():
@@ -27,31 +22,21 @@ def api_login():
         "serviceTicket": serviceTicket
     }
     body = {
+        # Must be admin account on Ruckus zd 144
         "username": os.getenv("API_USERNAME"),
         "password": os.getenv("API_PASSWORD")
     }
     try:
-
         response = requests.post(f"{api_base_url}/serviceTicket", headers=headers, json=body, verify=False)
 
         if response.status_code == 200:
             data = response.json()
             serviceTicket = data['serviceTicket']
-
-            # with open(st_file, 'w') as file:
-            #     file.write(serviceTicket)
-            # # print(f"Wrote serviceTicket {serviceTicket} to {st_file}")
-            # # print(f"Logged In")
-
-            # # print("Login / ServiceTicket Response Data")
-            # # print(data)
             return True
         else:
-            # # print(f"Not Logged In")
-            # # print(f"Error: {response.status_code} - {response.text}")
+            # Not logged in
             return False
-    except Exception as e:
-        # # print(f"An error occured {e}")
+    except:
         return False
 
 def api_logoff():
@@ -64,15 +49,7 @@ def api_logoff():
         "serviceTicket": serviceTicket
     }
     response = requests.delete(f"{api_base_url}/serviceTicket", headers=headers, params=uri_params, verify=False)
-
-    if response.status_code == 200:
-        pass
-        # # print("Logged off successfully.")
-        # # print(response)
-    else:
-        pass
-        # # print("Logoff unsuccessful.")
-
+    # Could write some code here to return a prtg error proper
 
 def get_rkzones():
     headers = {
@@ -82,26 +59,19 @@ def get_rkzones():
         uri_params = {
             "serviceTicket": serviceTicket
         }
-        # # print(f"params: {uri_params}")
         response = requests.get(f"{api_base_url}/rkszones", headers=headers, params=uri_params, verify=False)
 
         if response.status_code == 200:
             data = response.json()
-            # # print("# printing Zones")
-            # # print(data)
             return data
         else:
-            # print(f"Error: {response.status_code} - {response.text}")
             return False
-    except Exception as e:
-        # print(f"An error occured {e}")
+    except:
         return False
 
 def ap_query():
-
     full_list = []
     current_page = 1
-    first_index = 0
 
     headers = {
         "Content-Type": "application/json;charset=UTF-8"
@@ -126,31 +96,18 @@ def ap_query():
             response = requests.post(f"{api_base_url}/query/ap", headers=headers, params=uri_params, data=json.dumps(body), verify=False)
             resp_json = response.json()
             full_list.extend(resp_json['list'])
-
-            total_count = resp_json['totalCount']
             more = resp_json['hasMore']
-            ret_first_index = resp_json['firstIndex']
-            this_count = len(resp_json['list'])
 
             if more == True:
                 current_page += 1
-                # first_index += this_count
-                # # print(resp_json)
-                # print(f"We've got more to get. Onto page {current_page}!")
             else:
-                # print(more)
-                # print("All done!")
                 break
-
-        except Exception as ex:
-            # print("Exception")
-            # print(ex)
+        except:
             break
 
     return full_list
 
 success = api_login()
-# # print(serviceTicket)
 
 final_json = {
     "prtg": {
@@ -161,16 +118,13 @@ final_json = {
 }
 if success == True:
     zones = get_rkzones()
-    # # print(zones)
     for zone in zones['list']:
         if zone['name'] == "APS-Zone":
             apsZoneUID = zone['id']
-            # print(f"Zone Found: {apsZoneUID}")
     
     ap_query_response = ap_query()
 
     for device in ap_query_response:
-        # # print(f"{device['deviceName']}\t{device['ip']}\t{device['apMac']}.")
         result_dict = {
                     "channel": f"{device['deviceName']} 5Ghz Airtime",
                     "value": device['airtime5G'],
